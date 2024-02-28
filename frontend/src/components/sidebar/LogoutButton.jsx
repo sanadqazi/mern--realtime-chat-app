2b2d6b682eda0280ff268ec2a0646e5fac5eb6ac
Sanad Qazi <sanad.qazi@mirumagency.com>
2024-02-28 17:34:58 +0530

    Part 3 Completed

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { BiLogOut } from "react-icons/bi";
+import useLogout from '../../hooks/useLogout';
+
 
 const LogoutButton = () => {
+  const {loading, logout} = useLogout();
   return <div className='mt-auto'>
+    {!loading ? (<button onClick={logout} className='btn btn-ghost btn-sm rounded-btn' >
         <BiLogOut className='w-6 h-6 text-white cursor-pointer' />
+        </button>) : (
+          <span className='loading loading-spinner'></span>
+        )}
+    
         </div>;
 }
 
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SearchInput from './SearchInput'
-import Conversations from '../Conversations'
+import Conversations from './Conversations'
 import LogoutButton from './LogoutButton'
 
 const Sidebar = () => {
